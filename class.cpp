adb8920a1bd297ac04a30aadf4a3546b17dc6456
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-08-19 11:06:24 +0530

    First Commit

---
+++ class.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+   //Properties
+   string name;
+   float cgpa;
+   //Methods
+   void getPercentage(){
+    cout<<(cgpa*10)<<endl;
+   }
+};
+
+class User{
+    int id;
+    string username;
+    string bio;
+    void deactivate(){
+        cout<<"Deleting account";
+    }
+    void editBio(string newBio){
+        bio=newBio;
+    }
+};
+
+int main(){
+    Student s1;
+    cout<<sizeof(s1)<<endl;
+    return 0;
+}
