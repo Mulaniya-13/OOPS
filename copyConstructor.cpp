f601ef658c96d1a89739d53002b856a693c608ab
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-08-20 22:17:18 +0530

    updating existing files

--- copyConstructor.cpp
+++ copyConstructor.cpp
@@ -6,10 +6,13 @@ class Car {
     public:
         string name;
         string color;
+        int *mileage;
 
     Car(string name,string color){
         this->name=name;
         this->color=color;
+        mileage=new int;//dynamic allocation
+        *mileage=12;
     }
     
     //Custom copy constructor
@@ -17,14 +20,25 @@ class Car {
         cout<<"Copying old to new..\n";
         name=original.name;
         color=original.color;
+        // mileage=original.mileage;//shallow copy 
+        mileage=new int;
+        *mileage=*original.mileage;
     }
 };
 
 int main() {
     Car c1("Maruti 800","White");
 
-    Car c2(c1); //custom constructor
-    cout<<c2.name<<endl;//Maruti 800
-    cout<<c2.color<<endl;//White
+    // Car c2(c1); //custom constructor
+    // cout<<c2.name<<endl;//Maruti 800
+    // cout<<c2.color<<endl;//White
+
+    Car c2(c1);
+    cout<<c2.name<<endl;
+    cout<<c2.color<<endl;
+    cout<<*c2.mileage<<endl;
+    *c2.mileage=10;
+    cout<<*c1.mileage<<endl;//12
+    cout<<*c2.mileage<<endl;//10
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+Car(Car &original){
+    //     cout<<"Copying old to new..\n";
+    //     name=original.name;
+    //     color=original.color;
+    //     mileage=original.mileage;
+    // }
